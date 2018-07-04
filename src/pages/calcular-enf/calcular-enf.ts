14c80833f9c934f57efa57a274aa5ec371c0ecfa
Favio <faviog2@gmail.com>
2018-07-03 23:44:26 -0600

    agregar pagina de registrar enf

--- src/pages/calcular-enf/calcular-enf.ts
+++ src/pages/calcular-enf/calcular-enf.ts
@@ -33,6 +33,45 @@ export class CalcularEnfPage {
     total:null
   }
 
+  resp:respuestas = {
+    i1:null,
+    i2:null,
+    i3:null,
+    i4:null,
+    i5:null,
+    i6:null,
+    i7:null,
+    io1:null,
+    io2:null,
+    io3:null,
+    io4:null,
+    io5:null,
+    a1:null,
+    a2:null,
+    a3:null,
+    a4:null,
+    a5:null,
+    a6:null,
+    r1:null,
+    r2:null,
+    r3:null,
+    r4:null,
+    re1:null,
+    re2:null,
+    re3:null,
+    re4:null,
+    re5:null,
+    re6:null,
+    t1:null,
+    t2:null,
+    t3:null,
+    t4:null,
+    f1:null,
+    f2:null,
+    f3:null,
+    f4:null
+  }
+
   constructor(public navCtrl: NavController, public navParams: NavParams,
               public vars : VariablesProvider ) {
     this.enf.key = JSON.stringify(Date.now());
@@ -44,4 +83,43 @@ export class CalcularEnfPage {
     console.log('ionViewDidLoad CalcularEnfPage');
   }
 
+}
+
+class respuestas{
+  i1:number;
+  i2:number;
+  i3:number;
+  i4:number;
+  i5:number;
+  i6:number;
+  i7:number;
+  io1:number;
+  io2:number;
+  io3:number;
+  io4:number;
+  io5:number;
+  a1:number;
+  a2:number;
+  a3:number;
+  a4:number;
+  a5:number;
+  a6:number;
+  r1:number;
+  r2:number;
+  r3:number;
+  r4:number;
+  re1:number;
+  re2:number;
+  re3:number;
+  re4:number;
+  re5:number;
+  re6:number;
+  t1:number;
+  t2:number;
+  t3:number;
+  t4:number;
+  f1:number;
+  f2:number;
+  f3:number;
+  f4:number;
 }
