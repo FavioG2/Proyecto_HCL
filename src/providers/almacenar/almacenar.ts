6ae5c87e01a37c0dd910600217aa8ca239092f07
Favio <faviog2@gmail.com>
2019-03-07 22:04:48 -0600

    correcciones menores

--- src/providers/almacenar/almacenar.ts
+++ src/providers/almacenar/almacenar.ts
@@ -24,7 +24,7 @@ export class AlmacenarProvider {
     this.itemsCollection = this.afs.collection(coleccion);
     this.itemsCollection.doc(key).set(objeto).then(
       result => {
-        this.showAlert('Registro', 'Registro realizado exitosamente');
+        this.showAlert('Registro', 'Información guardada exitosamente');
       }
     ).catch(
       error => {
