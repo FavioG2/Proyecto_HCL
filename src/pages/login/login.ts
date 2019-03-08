18e0c22a7730feec5c0f2ca153d06aea68ad0793
Favio <faviog2@gmail.com>
2019-03-07 21:32:26 -0600

    Traducciones y correcciones

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -29,9 +29,10 @@ export class LoginPage {
       // El usuario se ha creado correctamente
     })
     .catch(err=>{
+      let mensaje = this.loginErrorHandler(err.code);
       let alert = this.alertCtrl.create({
         title: 'Error',
-        subTitle: err.message,
+        subTitle: mensaje,
         buttons: ['Aceptar']
       });
       alert.present();
@@ -39,15 +40,37 @@ export class LoginPage {
 
   }
 
+  loginErrorHandler(err){
+    if(err === 'auth/email-already-in-use'){
+      return "La dirección de correo ya ha sido usada por otra cuenta";
+    }else if(err === 'auth/weak-password'){
+      return "La contraseña debe tener al menos 6 caracteres"
+    }else if(err === 'auth/invalid-email'){
+      return "La dirección de correo electrónico es incorrecta"
+    }else if(err === 'auth/user-not-found'){
+      return "Los datos no coinciden con los de ningún usuario registrado"
+    }else if(err === 'auth/wrong-password'){
+      return "La contraseña es incorrecta"
+    }
+    else{
+      return "Error desconocido al registrar al usuario";
+    }
+  }
+
   login()
   {
     this.auth.loginUser(this.user.email,this.user.password ).then((user) => {
       }
     )
+
      .catch(err=>{
+      
+      console.log("ERROOOOOOR");
+      console.log(err); 
+      let mensaje = this.loginErrorHandler(err.code);
       let alert = this.alertCtrl.create({
         title: 'Error',
-        subTitle: err.message,
+        subTitle: mensaje,
         buttons: ['Aceptar']
       });
       alert.present();
